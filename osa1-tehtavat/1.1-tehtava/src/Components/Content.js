3db5a9a8f52c953540fb964be263f0b83b598b46
Supr0en <erik.ruotsalainen1@gmail.com>
2023-01-12 12:30:16 +0200

    korjauksia koodiin

--- osa1-tehtavat/1.1-tehtava/src/Components/Content.js
+++ osa1-tehtavat/1.1-tehtava/src/Components/Content.js
@@ -1,9 +1,11 @@
+import Part from './Part';
+
 const Content = ({ course }) => {
     return (
       <div>
-        <p> {course.parts[0].name} {course.parts[0].exercises}</p> 
-        <p> {course.parts[1].name} {course.parts[1].exercises}</p>
-        <p> {course.parts[2].name} {course.parts[2].exercises}</p>
+        <Part name={course.parts[0].name} exercises={course.parts[0].exercises} /> 
+        <Part name={course.parts[1].name} exercises={course.parts[1].exercises} />
+        <Part name={course.parts[2].name} exercises={course.parts[2].exercises} />
       </div>
     )
 }
---
+++ osa1-tehtavat/1.1-tehtava/src/Components/Part.js
@@ -0,0 +1,4 @@
+const Part = ({ name, exercises }) => {
+    return <p>{name} {exercises}</p>;
+}
+export default Part;
