068a8d2a439986601dcf9d17b788391c60dddeb2
Supr0en <erik.ruotsalainen1@gmail.com>
2023-01-27 16:23:07 +0200

    muutoksia testeihin

--- osa4-tehtavat/blogilista/tests/blog_api.test.js
+++ osa4-tehtavat/blogilista/tests/blog_api.test.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const helper = require('../utils/test_helper');
 const app = require('../app');
-const api = supertest(app)
-const bcrypt = require('bcrypt')
+const api = supertest(app);
 const User = require('../models/user')
 
 describe ('api tests', () => {
@@ -11,26 +10,26 @@ describe ('api tests', () => {
         title: 'post test using jest',
         author: 'Erik',
         url: 'http://localhost:3003/api/blogs',
-    }
+    };
 
     test('blogs are returned as json', async () => {
         await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    })
+    });
     
     test('check if all blogs work', async () => {
         const get = await api.get('/api/blogs');
         const count = get.body.length;
         const newCount = count + 1;
         expect(newCount).toBe(count + 1);
-    })
+    });
     
     test('identify blog with id', async () => {
         const get = await api.get('/api/blogs');
         expect(get.body[0].id).toBeDefined();
-    })
+    });
     
     test('post blog', async () => {
         const token = '';
@@ -43,21 +42,21 @@ describe ('api tests', () => {
 
         const blogAuthor = blogsAtEnd.map(blog => blog.author);
         expect(blogAuthor).toContain(newBlog.author);
-    })
+    });
     
     test('check if likes is null', async () => {
         await api.post('/api/blogs').send(newBlog).expect(201);
         const get = await api.get('/api/blogs');
         const createdBlog = get.body.find(blog => blog.url === newBlog.url);
         expect(createdBlog.likes).toBe(0);
-    })
+    });
     
     test('if tittle or url dosent exist', async () => {
         const addBlog = {
             author: 'Erik',
         }
         await api.post('/api/blogs').send(addBlog).expect(400);
-    })
+    });
 
     test('delete blog', async () => {
         const blogsAtStart = await helper.blogsInDb();
@@ -73,7 +72,7 @@ describe ('api tests', () => {
         
         const deletedBlog = blogsAtEnd.map(blog => blog.id);
         expect(deletedBlog).not.toContain(blogToDelete.id)
-    })
+    });
 
     test('update blog', async () => {
         const blogsAtStart = await helper.blogsInDb();
@@ -85,7 +84,7 @@ describe ('api tests', () => {
         const blogsAtEnd = await helper.blogsInDb();
         const updatedBlog = blogsAtEnd.find(blog => blog.id === blogToUpdate.id);
         expect(updatedBlog.likes).toBe(likes);
-    })
+    });
 })
 
 describe('when there is initially one user at db', () => {
@@ -108,7 +107,7 @@ describe('when there is initially one user at db', () => {
     
         const usersAtEnd = await helper.usersInDb()
         expect(usersAtEnd).toHaveLength(usersAtStart.length)
-    })
+    });
   
     test('creation succeeds with a fresh username', async () => {
         const usersAtStart = await helper.usersInDb()
@@ -130,7 +129,8 @@ describe('when there is initially one user at db', () => {
       
         const usernames = usersAtEnd.map(u => u.username)
         expect(usernames).toContain(newUser.username)
-    })
+    });
+
     test('creating user fails if user already exists', async () =>{
 
         const newUser = {
@@ -142,7 +142,22 @@ describe('when there is initially one user at db', () => {
         const userCount = get.body.length;
         await api.post('/api/users').send(newUser).expect(400);
         expect(userCount).not.toBe(userCount + 1);
-    })
+    });
+
+    test('creating new user if unauthorized', async () => {
+        const blogsAtStart = await helper.blogsInDb();
+
+        const newBlog = {
+            title: 'eitoimi',
+            author: 'toimii',
+            url: 'https://localhost:3003'
+        }
+    
+        await api.post('/api/blogs').send(newBlog).expect(401);
+        const blogsAtEnd = await helper.blogsInDb();
+
+        expect(blogsAtEnd).toHaveLength(blogsAtStart.length);
+    });
 })
 
 afterAll(async () => {
